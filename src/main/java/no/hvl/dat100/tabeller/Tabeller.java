d826babc4313a756ad7736e503703c1a716bf2c6
elidaelverum <676951@stud.hvl.no>
2024-09-24 12:22:33 +0200

    oppdater

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,16 +5,46 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-
-	}
-
+		int[] heltallTabell = {1,2,4,7};
+		for (int i = 0; i <= 4; i++) {
+			int v = heltallTabell[i];
+			
+			if (i < heltallTabell.length - 1) {
+				System.out.print(", ");
+				
+			}
+		
+			System.out.print(v);
+			
+		}
+		}
+	
 	// b)
 	public static String tilStreng(int[] tabell) {
+		
+		int[] tabell2 = {42, 67, 89};
+		int[] utskrift = tabell2;
+		
+		
+		
+		for (int j = 0; j < tabell2.length; j++) {
+			int w = tabell2[j];
+			
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+			System.out.print(utskrift);
+			
+			if ( j < tabell2.length -1) {
+				System.out.print(", ");
+				
+			}
+
+		}
+		
+		System.out.print("]");
+		return "";
+		
+
+		
 	}
 
 	// c)
