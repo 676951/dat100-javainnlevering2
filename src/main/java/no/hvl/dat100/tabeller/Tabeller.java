9322e3080be93581b17bd231d247aa3ac3a87569
678156 <678156@stud.hvl.no>
2024-09-25 13:52:15 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -4,102 +4,130 @@ import static java.lang.Integer.parseInt;
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
 
-
 public class Tabeller {
 
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		int[] heltallTabell = {1,2,4,7};
+		int[] heltallTabell = { 1, 2, 4, 7 };
 		for (int i = 0; i <= 4; i++) {
 			int v = heltallTabell[i];
-			
+
 			if (i > 0) {
 				System.out.print(", ");
-				
+
 			}
-		
+
 			System.out.print(v);
-	
+
 		}
 	}
-	
+
 	// b)
 	public static String tilStreng(int[] tabell) {
-		
 
-		String[] tabell1 = {"[42, ","67, ","89]"};
+		String[] tabell1 = { "[42, ", "67, ", "89]" };
 		for (String tall : tabell1)
 
-		System.out.print(tall);
+			System.out.print(tall);
 		System.out.print("\n");
-		
+
 		return null;
-		
+
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		int[] heltallTabell2 = {1,2,4,7};
+		int[] heltallTabell2 = { 1, 2, 4, 7 };
 		int sum = 0;
 		for (int i = 0; i < heltallTabell2.length; i++) {
-			
+
 			sum += heltallTabell2[i];
-			
-			}
+
+		}
 		System.out.print("Summen av tabellen er: " + sum);
 		System.out.print("\n");
-			
+
 		return sum;
 	}
-	
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		int [] tall2 = {3, 5, 8, 6};
+		int[] tall2 = { 3, 5, 8, 6 };
 		String tallTxt = showInputDialog("Gi et tall: ");
 		int leterEtter = parseInt(tallTxt);
-		
-		
+
 		boolean funnet = false;
 		int i = 0;
-		while ( !funnet && i < tall2.length) {
+		while (!funnet && i < tall2.length) {
 			if (tall2[i] == leterEtter) {
 				funnet = true;
-			}else {
+			} else {
 				i++;
-				
+
 			}
 		}
-		
-	if (funnet) {
-		System.out.print("True");
-		System.out.print("\n");
-	}else {
-		System.out.print("False");
-		System.out.print("\n");
-		
+
+		if (funnet) {
+			System.out.print("True");
+			System.out.print("\n");
+		} else {
+			System.out.print("False");
+			System.out.print("\n");
+
 		}
-	return !funnet && funnet;
+		return !funnet && funnet;
 	}
-	
-		
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		int[] tabellen = { 3, 2, 5, 7, 8 };
+		int i = 0;
+		int posisjonen = tabellen[i];
+
+		for (i = 0; i < tabellen.length; i++) {
+			if (i == 0) {
+				System.out.print(posisjonen);
+			} else {
+				System.out.print(", -1");
+			}
+
+		}
+		System.out.print("\n");
+		return -1;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+		int[] tabA = {1, 2, 3, 4, 5};
+		int[] revTab = new int [tabA.length];
+		
+		for (int i = 0; i < tabA.length; i++) {
+		revTab[i] = tabA[tabA.length -1 -i];
+		}
+		
+		for (int num : tabA) {
+			System.out.print(num);
+			
+		}
+		
+		System.out.println();
+		
+		for (int num : revTab) {
+		System.out.print(num);
+		}
+		System.out.print("\n");
+		return revTab;
 	}
+	
+	
+	
+
+	
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
