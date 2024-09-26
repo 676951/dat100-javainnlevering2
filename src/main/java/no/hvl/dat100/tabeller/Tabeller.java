a55928024ca9d9450c73df6d853eebf570f94da2
LeneJ <186477@stud.hvl.no>
2024-09-26 21:10:51 +0200

    UpdateTabeller

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -9,45 +9,54 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		int[] heltallTabell = { 1, 2, 4, 7 };
-		for (int i = 0; i <= 4; i++) {
-			int v = heltallTabell[i];
+		String outputA = "";
+
+		for (int i = 0; i < tabell.length; i++) {
+			int v = tabell[i];
 
 			if (i > 0) {
-				System.out.print(", ");
+				outputA += ", ";
 
 			}
 
-			System.out.print(v);
+			outputA += v;
 
 		}
+
+		System.out.println("Oppgave A: " + outputA);
+
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		String[] tabell1 = { "[42, ", "67, ", "89]" };
-		for (String tall : tabell1)
+		String outputB = "[";
 
-			System.out.print(tall);
-		System.out.print("\n");
+		for (int i = 0; i < tabell.length; i++) {
+			outputB += tabell[i];
 
-		return null;
+			if (i < tabell.length - 1) {
+				outputB += ",";
+			}
+		}
+
+		outputB += "]";
+
+		System.out.println("Oppgave B: " + outputB);
+		return outputB;
 
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		int[] heltallTabell2 = { 1, 2, 4, 7 };
 		int sum = 0;
-		for (int i = 0; i < heltallTabell2.length; i++) {
+		for (int i = 0; i < tabell.length; i++) {
 
-			sum += heltallTabell2[i];
+			sum += tabell[i];
 
 		}
-		System.out.print("Summen av tabellen er: " + sum);
-		System.out.print("\n");
+		System.out.println("Oppgave C: " + sum);
 
 		return sum;
 	}
@@ -55,92 +64,89 @@ public class Tabeller {
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		int[] tall2 = { 3, 5, 8, 6 };
-		String tallTxt = showInputDialog("Gi et tall: ");
-		int leterEtter = parseInt(tallTxt);
-
 		boolean funnet = false;
 		int i = 0;
-		while (!funnet && i < tall2.length) {
-			if (tall2[i] == leterEtter) {
+
+		while (!funnet && i < tabell.length) {
+			if (tabell[i] == tall) {
 				funnet = true;
 			} else {
 				i++;
 
 			}
-		}
-
-		if (funnet) {
-			System.out.print("True");
-			System.out.print("\n");
-		} else {
-			System.out.print("False");
-			System.out.print("\n");
 
 		}
-		return !funnet && funnet;
+		System.out.println("Oppgave D: " + funnet);
+		return funnet;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		int[] tabellen = { 3, 2, 5, 7, 8 };
-		int i = 0;
-		int posisjonen = tabellen[i];
-
-		for (i = 0; i < tabellen.length; i++) {
-			if (i == 0) {
-				System.out.print(posisjonen);
-			} else {
-				System.out.print(", -1");
+		for (int posisjon = 0; posisjon < tabell.length; posisjon++) {
+			if (tabell[posisjon] == tall) {
+				System.out.println("Oppgave E: " + posisjon);
+				return posisjon;
 			}
 
 		}
-		System.out.print("\n");
+		System.out.println("Oppgave E: -1");
 		return -1;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
 
-		int[] tabA = {1, 2, 3, 4, 5};
-		int[] revTab = new int [tabA.length];
-		
-		for (int i = 0; i < tabA.length; i++) {
-		revTab[i] = tabA[tabA.length -1 -i];
+		int[] revTab = new int[tabell.length];
+
+		for (int i = 0; i < tabell.length; i++) {
+			revTab[i] = tabell[tabell.length - 1 - i];
 		}
-		
-		for (int num : tabA) {
-			System.out.print(num);
-			
-		}
-		
-		System.out.println();
-		
-		for (int num : revTab) {
-		System.out.print(num);
-		}
-		System.out.print("\n");
+
+		System.out.println("Oppgave F: " + finTekst(revTab)); 
 		return revTab;
 	}
-	
-	
-	
-
-	
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erSortert ikke implementert");
+		for (int i = 1; i < tabell.length; i++) {
+			if (tabell[i] < tabell[i - 1]) {
+				System.out.println("Oppgave G: Tabellen er ikke sortert");
+				return false;
+			}
+		}
+		System.out.println("Oppgave G: Tabellen er sortert");
+		return true;
+
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
+		int[] tabell3 = new int[tabell1.length + tabell2.length];
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
+		for (int i=0; i<tabell1.length;i++) {
+			tabell3[i]=tabell1[i];
+		}
+		for (int i=0; i<tabell2.length;i++) {
+			tabell3[i+tabell1.length]=tabell2[i];
+		}
+
+		System.out.println("Oppgave H: " + finTekst(tabell3));
+		return tabell3;
 
 	}
+	
+	// Lager en metode for å printe hele tabellen (gjør det samme som oppgave b)
+		public static String finTekst(int[] tabell) {
+			String tabellFin = "[";
+			for (int i = 0; i < tabell.length; i++) {
+				tabellFin += tabell[i];
+				if (i < tabell.length - 1) {
+					tabellFin += ",";
+				}
+			}
+			tabellFin += "]";
+			return tabellFin;
+		}
 }
